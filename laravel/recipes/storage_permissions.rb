f1da9e44a33984f4ed29046aec30359eaf64848e
Alan <alan@paradojo.com>
2016-04-26 23:14:57 -0500

    Update storage_permissions.rb

--- laravel/recipes/storage_permissions.rb
+++ laravel/recipes/storage_permissions.rb
@@ -4,7 +4,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    chmod -R 777 storage/app storage/framework storage/logs
+    chmod -R 777 storage/app storage/framework storage/logs bootstrap
     EOH
   end
 end
